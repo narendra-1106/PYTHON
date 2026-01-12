8df18de022ab5fae02b28144f68455707415047b
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-12 15:02:17 +0530

    3rd

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+# print(15+5) # Addition
+# print(15-5) # Sub
+# print(15*5) # Multiplication
+# print(15/5) # div
+# print(15//5) # Floor division
+# print(15%5) # Modulus Show remender
+# print(2**5) # Power 
+
+a=10
+b=5
+print(a+b) # Addition
+print(a-b) # Sub
+print(a*b) # Multiplication
+print(a/b) # div
+print(a//b) # Floor division
+print(a%b) # Modulus Show remender
+print(a**b) # Power 
