dbab6a3405127a4ff30d0acdee98119ac4367c00
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-12 12:18:17 +0530

    Day 2

---
+++ 2nd.py
@@ -0,0 +1,40 @@
+print("Narendra Jagtap")
+print(8657557138)
+print("This is my first program")
+print(69*69)
+print("This statement print next line \nThis is next line \nThis is escape sequence") 
+# \n this is used for next line and its khown as escape sequence
+# when we need to comment 2 to3 lines use (ctrl + /) 
+''' This 3quotes are used for 
+multi- line comment
+we can also use 3 double quotes for multi - line comment'''
+
+print("My name is \"Narendra\" ") #this is also escape sequence
+
+print("Narendra ", 6 , 9, sep=":" , end =" 225\n")
+print("Jagtap")
+
+# Variable and data Types
+a="Naru"
+print(a)
+
+b="Narendra"
+print(b)
+
+c=143.225
+print(c)
+
+d=1  #integer
+e=True  #boolean
+f="Narendra" #string
+g=None #None Type variable 
+
+# All this are data types
+
+print("Type of A is", type(d))
+print("Type of A is", type(e))
+print("Type of A is", type(f))
+print("Type of A is", type(g))
+print("Type of A is", type(c))
+
+#Built in data types
