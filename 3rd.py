51ebfb03c5b489c42b7e9c256cdb6c5eb1c6854b
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-13 18:47:07 +0530

    Day 4

--- 3rd.py
+++ 3rd.py
@@ -1,2 +1,10 @@
 # Typecasting : converting one data type into another data type
 
+a= input("Enter a first number : ")
+b= input("Enter a second number : ")
+c=("Addition : " , int(a)+ int(b))
+print(c)
+print(int(a)+int(b))
+print(a+b)
+# print(int(a)+int(b))
+# print(int(a)+int(b))
---
+++ 4th.py
@@ -0,0 +1,9 @@
+#strings 
+name= "Narendra"
+surname= " jagtap"
+NJ= '''Narendra
+jagtap
+age: 18
+Narendra jagtap'''
+
+print("Hello ," + name)
