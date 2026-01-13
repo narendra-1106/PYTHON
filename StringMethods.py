37f28537eb54afbd6becb5098741da7889d62cae
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-13 22:05:04 +0530

    Add string methods and slicing example scripts
    Added StringMethods.py demonstrating various string methods such as upper, lower, rstrip, replace, split, capitalize, center, and count. Added StringSlicing.py to illustrate string slicing and iteration over characters.

---
+++ StringMethods.py
@@ -0,0 +1,21 @@
+#Strings are immutable
+
+a="Narendra &&& !!! Narendra"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!")) #delete trailing char
+
+print(a.replace("Narendra", "Naru"))
+
+print(a.split(" "))
+
+Name= "nArEnDRa jAgtaP "
+print(Name.capitalize()) #first char upper case and other lower case
+
+
+print(len(a))
+print(len(a.center(50)))
+
+print(a.count("Narendra"))
+
---
+++ StringSlicing.py
@@ -0,0 +1,9 @@
+#String slicing
+
+Names="Narendra,Jagtap"
+print(Names[0:8])
+print(Names[0:-7])
+print(len(Names))
+
+for N in Names:
+    print(N)
