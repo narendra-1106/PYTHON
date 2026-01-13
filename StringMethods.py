03ccc3f15833ffa82b6b533d8a615321d48c284a
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-13 22:34:12 +0530

    Add examples of string method usages
    Added multiple print statements demonstrating the use of various string methods such as endswith, find, isalnum, isalpha, islower, isupper, isprintable, istitle, swapcase, and title. These examples help illustrate the behavior and output of each method.

--- StringMethods.py
+++ StringMethods.py
@@ -19,3 +19,37 @@ print(len(a.center(50)))
 
 print(a.count("Narendra"))
 
+print(Name.endswith("!!!"))  #TRUE or False
+
+print(Name.find("j"))
+
+#Insdex Method 
+# print(Name("nArEnDRa"))
+
+
+Name1="NarendraJagtap"
+#is alnum means used of A-z and 1-infi 
+print(Name1.isalnum())
+
+name="naru1106"
+#isalpha used for only A-z Not for Numbers
+print(Name1.isalpha())
+print(name.isalpha())
+
+print(name.islower())
+print(name.isupper())
+
+str="Narendra Jagtap"
+str1="Narendra \njagtap"
+print(str.isprintable())
+print(str1.isprintable())
+
+
+#is title is return true only when first letter is capital
+print(str.istitle())
+
+#swap case is used for upper to lower and lower to upper
+print(str.swapcase())
+
+#used for title  capitalize the first letter
+print(str.title())
