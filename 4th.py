efece9485df4dacc7936b2506f69479c222d1d03
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-13 18:54:11 +0530

    Update 4th.py

--- 4th.py
+++ 4th.py
@@ -7,3 +7,17 @@ age: 18
 Narendra jagtap'''
 
 print("Hello ," + name)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+# print(name[8]) This gives an error 8th char are not exist
+
+print("\nUsing for loop \n")
+for character in NJ:
+    print(character)
