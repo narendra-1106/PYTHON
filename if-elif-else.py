375fe91d0fec413a187bb6b0f037d16affacc416
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-14 14:34:54 +0530

    Add if-elif-else number classification script
    Introduces a Python script that classifies user input as positive, zero, negative, or special cases (100, -225) using if-elif-else statements.

---
+++ if-elif-else.py
@@ -0,0 +1,15 @@
+num=int(input("Enter a number: "))
+if(num>0):
+    print("Num is positive")
+
+elif(num==0):
+    print("Number is zero")
+
+elif(num==100):
+    print("Num not exist")
+
+elif(num== -225):
+    print("Num is special")
+
+else:
+    print("Num is negative")
