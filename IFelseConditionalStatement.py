d9b92fb27de157139dc9827252403f4fa73eb42a
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-14 09:58:30 +0530

    Add example of if-else conditional statement in Python
    Introduces a script demonstrating the use of if-else statements and conditional operators to check age and print eligibility for driving.

---
+++ IFelseConditionalStatement.py
@@ -0,0 +1,19 @@
+#if else statement
+
+a=int(input("Enter your age:"))
+print("Your age is ", a)
+
+if(a>18):
+    print("You can Drive a car")  #space= indentation
+
+else:
+    print("You cannot drive a car")
+
+
+#This is conditional operators
+print(a>18)
+print(a>=18)
+print(a<18)
+print(a<=18)
+print(a==18)
+print(a!=18)
