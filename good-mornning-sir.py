8496ac2c6553273b021b259c16361c569ab6895a
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-14 23:20:11 +0530

    Add script to display current time components
    Introduces good-mornning-sir.py, which prints the current time in HH:MM:SS format and separately outputs the hour, minute, and second.

---
+++ good-mornning-sir.py
@@ -0,0 +1,11 @@
+import time
+timeS=time.strftime("%H:%M:%S")
+print(timeS)
+
+timeS=time.strftime("%H")
+print(timeS)
+timeS=time.strftime("%M")
+print(timeS)
+timeS=time.strftime("%S")
+print(timeS)
+
