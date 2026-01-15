f62f8f5777a2177f4bb5b637dba9bb9157ecbdd0
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-15 20:18:26 +0530

    Add while loop examples in Python
    Introduces a new file with various commented and active while loop examples, demonstrating basic loop constructs and user input handling.

---
+++ while-loop.py
@@ -0,0 +1,23 @@
+# i=0
+# while (i<3):
+#     print(i)
+#     i=i+1
+
+
+# i=int(input("Enter number :"))
+
+# while(i<=100):
+#     i=int(input("Enter number :"))
+#     print(i)
+
+# print("Done with loop")
+
+# c=5
+# while(c>0):
+#     print(c)
+#     c=c-1
+
+c=1
+while(c>0):
+    print(c)
+    c=c+1
