e4028a4e5ad0de133c2ff1066d84f7579f4f4f52
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-15 20:08:10 +0530

    Add examples of for loops in Python
    Introduces a new script demonstrating various for loop usages, including iterating over strings, lists, and ranges, as well as nested loops and user input.

---
+++ for-loops.py
@@ -0,0 +1,19 @@
+name="Narendra"
+for i in name:
+    print(i)
+    
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for x in colors:
+    print(x)
+    for i in colors:
+        print(i)
+
+
+x=int(input("Enter a value of x : "))
+for k in range (x):
+    print(k)
+
+
+for k in range (3, 100, 3):
+    print(k)
