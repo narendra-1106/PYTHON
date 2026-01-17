aee8aaef01f6fbc95f2eacf474858c4717d2c084
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-17 19:51:58 +0530

    Add geometric mean and comparison functions
    Implemented calculategeometricmean to compute and print the geometric mean of two numbers, and isGreater to compare two numbers and print which is greater. Added example calls for both functions. islesser function is defined but not yet implemented.

---
+++ functions.py
@@ -0,0 +1,33 @@
+def calculategeometricmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=9
+b=6
+# geometricmean=(a*b)/(a+b)
+calculategeometricmean(a,b)
+c=8
+d=3
+# print(geometricmean)
+# geometricmean1=(c*d)/(c+d)
+# print(geometricmean1)
+calculategeometricmean(c,d)
+
+
+
+def islesser(a,b):
+    pass
+
+
+def isGreater(a,b):
+    if(a>b):
+        print("first no is greater")
+
+    else:
+        print("second no is greater or equal")
+
+isGreater(a,b)
+isGreater(c,d)
+
+
+
