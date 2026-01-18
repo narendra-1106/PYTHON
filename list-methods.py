d11fb16d6e7851fc680ff918f476559d0a75e1cf
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-18 14:21:08 +0530

    Create list-methods.py

---
+++ list-methods.py
@@ -0,0 +1,16 @@
+l=[5,2,9,12,4,2]
+print(l)
+l.append(7)
+# l.sort(reverse=True)
+# l.sort()
+# l.reverse()
+print(l.index(2))
+# print(l.count(2))
+print(l)
+
+# m=l.copy() as it is
+m=l
+m[0]=0
+print(l)
+l.insert(1 ,99)
+print(l)
