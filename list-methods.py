d198db4c50bfc1865b7711b38f9cd3812daf2eba
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-18 14:56:37 +0530

    Update list-methods.py

--- list-methods.py
+++ list-methods.py
@@ -9,8 +9,14 @@ print(l.index(2))
 print(l)
 
 # m=l.copy() as it is
-m=l
-m[0]=0
-print(l)
-l.insert(1 ,99)
-print(l)
+# m=l
+# m[0]=0
+# print(l)
+# l.insert(1 ,99)
+# print(l)
+
+
+m=[6,4,6,76,8,65]
+k=l+m
+# l.extend(m)
+print(k)
