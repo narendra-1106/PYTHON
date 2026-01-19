bd969ccdd47ecffd06b5063d4891811457225dda
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-19 19:47:54 +0530

    Update good-morning-sir-program.py

--- good-morning-sir-program.py
+++ good-morning-sir-program.py
@@ -1,17 +1,20 @@
+name= input(" Enter Your Name :")
+print(name)
+
 import time
 t=time.strftime("%H, %M, %S")
 hour= int(time.strftime("%H"))
 print(hour)
 
 if(hour>0 and hour<12):
-    print("Good Mornning Sir")
+    print("Good Mornning ",name)
 
 elif(hour>12 and hour<17):
-    print("Good Afternoon Sir")
+    print("Good Afternoon " ,name)
 
 elif(hour>17 and hour<21):
-    print("Good Evinning Sir")
+    print("Good Evinning ",name)
 
 else:
-    print("Good Night Sir")
+    print("Good Night ",name)
 
