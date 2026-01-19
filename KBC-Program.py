c96d2295cca7e647969d18527c5afb96878ede6d
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-19 19:22:56 +0530

    Add KBC quiz program and remove temp runner file
    Added a new Python script 'KBC-Program.py' that implements a simple KBC-style quiz game. Removed 'tempCodeRunnerFile.py', which contained a time-based greeting script.

---
+++ KBC-Program.py
@@ -0,0 +1,15 @@
+print("Lets Play KBC With Narendra")
+question = ("Who was the mother of Chhatrapati Shivaji Maharaj ? ")
+options = ("A.Tarabai\nB.Jijabai\nC.Ahilyabai\nD.Putalabai ")
+print(question)
+print(options)
+
+ans=input("Enter correct option :")
+
+if(ans=="B"):
+    print("Your Ans is correct you won 1Crore Ruppes")
+
+elif(ans=="b"):
+    print("Your Ans is correct you won 1Crore Ruppes")
+else:
+    print("Ans is Incorrect , get out KBC , Tuzi layki nahi aahe khelaychi")
