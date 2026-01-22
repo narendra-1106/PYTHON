17a94aa95728a03a4bdbb781615c066f53f23dc2
narendra-1106 <narendra1jagtap@gmail.com>
2026-01-22 17:18:21 +0530

    Add script to raise custom ValueError on input
    Introduces a Python script that prompts the user for a number between 1 and 10 and raises a ValueError if the input is out of range.

---
+++ raising-custon-errors.py
@@ -0,0 +1,15 @@
+a  =int(input("Enter a number between 1 to 10 : "))
+
+# a=quit
+# print("This is correct input")
+
+if(a<1 or a>10):
+    raise ValueError("Value should be between 1 and 10 ")
+    
+# for i is quit:
+#     print( i,"This is correct input")
+    
+
+
+
+
